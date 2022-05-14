8aed7c7cf4a6190c4a61a03b4739e79801db6aa2
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-05-14 18:28:51 +0200

    refactor: Refactor code and styles

--- src/data/gradient.js
+++ src/data/gradient.js
@@ -1,39 +1,69 @@
-// background-image: linear-gradient(gradient);
+// https://webgradients.com/
 
 export const linearGradient = [
-  { gradient: "to right, #fa709a 0%, #fee140 100%" },
-  { gradient: "to right, #43e97b 0%, #38f9d7 100%" },
-  { gradient: "to top, #30cfd0 0%, #330867 100%" },
-  { gradient: "to top, #5ee7df 0%, #b490ca 100%" },
-  { gradient: "to top, #fddb92 0%, #d1fdff 100%" },
-  { gradient: "to top, #96fbc4 0%, #f9f586 100%" },
+  { gradient: "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)" },
   {
     gradient:
-      "to right, #e4afcb 0%, #b8cbb8 0%, #b8cbb8 0%, #e2c58b 30%, #c2ce9c 64%, #7edbdc 100%",
+      "linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)",
+  },
+  { gradient: "linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%)" },
+  { gradient: "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)" },
+  { gradient: "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)" },
+  { gradient: "linear-gradient(120deg, #f093fb 0%, #f5576c 100%)" },
+  { gradient: "linear-gradient(to right, #fa709a 0%, #fee140 100%)" },
+  { gradient: "linear-gradient(to top, #30cfd0 0%, #330867 100%)" },
+  { gradient: "linear-gradient(to top, #fddb92 0%, #d1fdff 100%)" },
+  { gradient: "linear-gradient(to top, #96fbc4 0%, #f9f586 100%)" },
+  { gradient: "linear-gradient(180deg, #2af598 0%, #009efd 100%)" },
+  {
+    gradient:
+      "linear-gradient(to right, #e4afcb 0%, #b8cbb8 0%, #b8cbb8 0%, #e2c58b 30%, #c2ce9c 64%, #7edbdc 100%)",
   },
   {
     gradient:
-      "to right, #b8cbb8 0%, #b8cbb8 0%, #b465da 0%, #cf6cc9 33%, #ee609c 66%, #ee609c 100%",
+      "linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%)",
   },
-  { gradient: "to top, #9795f0 0%, #fbc8d4 100%" },
+  { gradient: "linear-gradient(to top, #9795f0 0%, #fbc8d4 100%)" },
   {
     gradient:
-      "to top, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%",
+      "linear-gradient(to top, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%)",
   },
-  { gradient: "to top, #f43b47 0%, #453a94 100%" },
-  { gradient: "to top, #0250c5 0%, #d43f8d 100%" },
-  { gradient: "15deg, #13547a 0%, #80d0c7 100%" },
-  { gradient: "to top, #ff0844 0%, #ffb199 100%" },
-  { gradient: "to top, #0c3483 0%, #a2b6df 100%, #6b8cce 100%, #a2b6df 100%" },
-  { gradient: "to right, #f83600 0%, #f9d423 100%" },
-  { gradient: "-20deg, #d558c8 0%, #24d292 100%" },
-  { gradient: "60deg, #abecd6 0%, #fbed96 100%" },
-  { gradient: "to top, #f77062 0%, #fe5196 100%" },
-  { gradient: "-60deg, #ff5858 0%, #f09819 100%" },
-  { gradient: "-60deg, #16a085 0%, #f4d03f 100%" },
-  { gradient: "to top, #e8198b 0%, #c7eafd 100%" },
-  { gradient: "to top, #0fd850 0%, #f9f047 100%" },
-  { gradient: "60deg, #3d3393 0%, #2b76b9 37%, #2cacd1 65%, #35eb93 100%" },
-  { gradient: "-225deg, #FF057C 0%, #8D0B93 50%, #321575 100%" },
-  { gradient: "-225deg, #231557 0%, #44107A 29%, #FF1361 67%, #FFF800 100%" },
+  {
+    gradient:
+      "linear-gradient(to top, #fcc5e4 0%, #fda34b 15%, #ff7882 35%, #c8699e 52%, #7046aa 71%, #0c1db8 87%, #020f75 100%)",
+  },
+  {
+    gradient:
+      "linear-gradient(to top, #dbdcd7 0%, #dddcd7 24%, #e2c9cc 30%, #e7627d 46%, #b8235a 59%, #801357 71%, #3d1635 84%, #1c1a27 100%)",
+  },
+  { gradient: "linear-gradient(to top, #f43b47 0%, #453a94 100%)" },
+  {
+    gradient:
+      "linear-gradient(to top, #4fb576 0%, #44c489 30%, #28a9ae 46%, #28a2b7 59%, #4c7788 71%, #6c4f63 86%, #432c39 100%)",
+  },
+  { gradient: "linear-gradient(to right, #f83600 0%, #f9d423 100%)" },
+  { gradient: "linear-gradient(-20deg, #d558c8 0%, #24d292 100%)" },
+  { gradient: "linear-gradient(-60deg, #16a085 0%, #f4d03f 100%)" },
+  {
+    gradient: "linear-gradient(to top, #3b41c5 0%, #a981bb 49%, #ffc8a9 100%)",
+  },
+  { gradient: "linear-gradient(to top, #0fd850 0%, #f9f047 100%)" },
+  { gradient: "linear-gradient(to top, #d5dee7 0%, #ffafbd 0%, #c9ffbf 100%)" },
+  { gradient: "linear-gradient(to right, #00dbde 0%, #fc00ff 100%)" },
+  { gradient: "linear-gradient(to right, #f9d423 0%, #ff4e50 100%)" },
+  { gradient: "linear-gradient(to top, #50cc7f 0%, #f5d100 100%)" },
+  { gradient: "linear-gradient(to top, #e14fad 0%, #f9d423 100%)" },
+  {
+    gradient: "linear-gradient(-225deg, #FF057C 0%, #7C64D5 48%, #4CC3FF 100%)",
+  },
+  {
+    gradient:
+      "linear-gradient(-225deg, #231557 0%, #44107A 29%, #FF1361 67%, #FFF800 100%)",
+  },
+  {
+    gradient: "linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%)",
+  },
+  {
+    gradient: "linear-gradient(-225deg, #FF3CAC 0%, #562B7C 52%, #2B86C5 100%)",
+  },
 ];
